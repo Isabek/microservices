94327965b323943b25ae03749ed9db1d76c79056
Isabek <isabek2309@gmail.com>
2016-08-03 02:30:30 +0300

    - Changed paths.

--- travis/install/01-install-gateway-jhipster-dependencies.sh
+++ travis/install/01-install-gateway-jhipster-dependencies.sh
@@ -5,5 +5,25 @@ set -ev
 npm install -g bower
 npm install -g gulp-cli
 
+ls -la
+
+cd ..
+ls -la
+
+cd ..
+ls -la
+
+
+cd ..
+ls -la
+
+
+cd ..
+ls -la
+
+cd ..
+ls -la
+
+
 cd ${HOME}/Isabek/microservices/gateway
 npm install
